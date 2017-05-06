0d9df8bcaa06aa82ebd4a42fa9c50ffdad9d9ae4
psychobolt <psychobolt@gmail.com>
2017-05-05 17:19:14 -0700

    Update ESLint rules

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,10 @@
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
+
 const { JSDOM } = jsdom;
 
 global.fetch = require('node-fetch');
 
+/* eslint-disable no-new */
 new JSDOM('<!doctype html><html><body></body></html>');
 
 require('babel-polyfill');
