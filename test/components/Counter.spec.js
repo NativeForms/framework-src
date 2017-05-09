7bf68b42391a14d40ed273b828acf93385a9c9d8
psychobolt <psychobolt@gmail.com>
2017-05-08 19:12:29 -0700

    Enable Counter component test

--- test/components/Counter.spec.js
+++ test/components/Counter.spec.js
@@ -14,28 +14,28 @@ const props = {
   incrementAsync: sinon.spy()
 };
 
-test.skip('components <CounterComponent />', function spec() {
-  this.timeout(5000);
-
+describe('components <CounterComponent />', function spec() {
   it('should render correctly', () => {
-    const wrapper = shallow(<CounterComponent {...props} />);
-    expect(wrapper.find(View)).to.have.length(1);
-    const view = wrapper.find(View);
-    expect(view.find(Text)).to.have.length(5);
-    expect(view.find(Text).node.props.children).to.eql(['Clicked: ', 1, ' times']);
-    const touchs = view.find(TouchableHighlight);
-    expect(touchs).to.have.length(4);
-    ['+', '-', 'Increment if odd', 'Increment async'].forEach((text, i) => {
-      expect(touchs.at(i).find(Text).node.props.children).to.equal(text);
-    });
-  });
-
-  ['increment', 'decrement', 'incrementIfOdd', 'incrementAsync']
-    .forEach((func, i) => {
-      it(`should call ${func} with TouchableHighlight on press`, () => {
-        const wrapper = shallow(<CounterComponent {...props} />);
-        wrapper.find(View).find(TouchableHighlight).nodes[i].props.onPress();
-        expect(props[func].calledOnce).to.be.true;
+    setTimeout(() => {
+      const wrapper = shallow(<CounterComponent {...props} />);
+        expect(wrapper.find(View)).to.have.length(1);
+        const view = wrapper.find(View);
+        expect(view.find(Text)).to.have.length(5);
+        expect(view.find(Text).node.props.children).to.eql(['Clicked: ', 1, ' times']);
+        const touchs = view.find(TouchableHighlight);
+        expect(touchs).to.have.length(4);
+        ['+', '-', 'Increment if odd', 'Increment async'].forEach((text, i) => {
+        expect(touchs.at(i).find(Text).node.props.children).to.equal(text);
       });
+
+      ['increment', 'decrement', 'incrementIfOdd', 'incrementAsync']
+        .forEach((func, i) => {
+          it(`should call ${func} with TouchableHighlight on press`, () => {
+            const wrapper = shallow(<CounterComponent {...props} />);
+            wrapper.find(View).find(TouchableHighlight).nodes[i].props.onPress();
+            expect(props[func].calledOnce).to.be.true;
+          });
+        });
     });
+  }, 5000);
 });
