6a08ddb2f31fcf099b3b215be337aa2b9beb0d89
dlin345 <dlin345@gmail.com>
2017-05-16 22:53:31 -0700

    Fixed spacing in Link Component

--- src/Framework/Core/Link/Link.component.js
+++ src/Framework/Core/Link/Link.component.js
@@ -2,31 +2,31 @@ import React, { Component, PropTypes } from 'react';
 import { Text, View, TouchableHighlight, Linking } from 'react-native';
 
 class LinkComponent extends Component {
-	static propTypes = {
-		url: React.PropTypes.string,
-	}
+  static propTypes = {
+    url: React.PropTypes.string,
+  }
 
-	handleURLClick = () => {
-		Linking.canOpenURL(this.props.url).then(supported => {
-			if (supported) {
-				Linking.openURL(this.props.url);
-			} else {
-				console.log('Cannot open URI: ' + this.props.url);
-			}
-		});
-	}
+  handleURLClick = () => {
+    Linking.canOpenURL(this.props.url).then(supported => {
+      if (supported) {
+        Linking.openURL(this.props.url);
+      } else {
+        console.log('Cannot open URI: ' + this.props.url);
+      }
+    });
+  }
 
-	render() {
-		const { url } = this.props;
+  render() {
+    const { url } = this.props;
 
-		return (
-			<TouchableHighlight onPress={this.handleURLClick}>
-				<View>
-					<Text>{url}</Text>
-				</View>
-			</TouchableHighlight>
-		);
-	}
+    return (
+      <TouchableHighlight onPress={this.handleURLClick}>
+        <View>
+          <Text>{url}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
 }
 
 export default LinkComponent;
