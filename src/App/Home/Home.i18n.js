ee0ba34f749167334c0892b72fe7c7e7fb1f3d0f
psychobolt <psychobolt@gmail.com>
2017-05-20 18:34:02 -0700

    Add I18n Support

---
+++ src/App/Home/Home.i18n.js
@@ -0,0 +1,6 @@
+export default {
+  en: {
+    welcome: 'Welcome to React Native Boilerplate!',
+    title: 'Home'
+  }
+};
---
+++ src/Framework/Counter/Counter.i18n.js
@@ -0,0 +1,8 @@
+export default {
+  en: {
+    incrementIfOdd: 'Increment if odd',
+    incrementAsync: 'Increment async',
+    clickedMessage: 'Clicked: {count, plural, one {# time} other {# times}}',
+    title: 'Counter'
+  }
+};
