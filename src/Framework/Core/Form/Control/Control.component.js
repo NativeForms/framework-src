a86bec0dbd5566187a617267723e6f4fce5c4d09
dlin345 <dlin345@gmail.com>
2017-05-27 13:42:16 -0700

    Added Control Component, updated Core directory

---
+++ src/Framework/Core/Form/Control/Control.component.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react';
+import { View } from 'react-native';
+
+// app
+import ButtonComponent from './Button/Button.component';
+import LinkComponent from './Link/Link.component';
+
+const CONTROL_MAP = {
+  button: 'renderButton',
+  link: 'renderLink',
+};
+
+export default class ControlComponent extends Component {
+  static propTypes = {
+    label: React.PropTypes.string.isRequired,
+    type: React.PropTypes.string.isRequired
+  }
+
+  renderControl() {
+    const controlType = this.props.type;
+    const func = this[CONTROL_MAP[controlType]].bind(this);
+    return func();
+  }
+
+  renderButton() {
+    return (
+      <View>
+        <ButtonComponent label={this.props.label} />
+      </View>
+    );
+  }
+
+  renderField() {
+    return (
+      <View>
+        <LinkComponent label={this.props.label} route='http://www.google.com' />
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View>
+        {
+          this.renderControl()
+        }
+      </View>
+    );
+  }
+}
