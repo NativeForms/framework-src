52d0b961be386a4574be5e36edb2c80fab47d553
psychobolt <psychobolt@gmail.com>
2017-05-29 13:40:42 -0700

    Add Link Container and route

---
+++ src/Framework/Core/Form/Control/Link/Link.container.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// framework
+import { Form } from '../../../Form';
+import schema from './Link.schema';
+import i18n from './Link.i18n';
+import { getLocale } from '../../../../../Shared/utils';
+
+const messages = i18n[getLocale()];
+
+export default class LinkContainer extends Component {
+  static navigationOptions = {
+    drawerLabel: messages.title,
+    title: messages.title,
+  }
+
+  render() {
+    return <Form schema={schema} messages={messages} />;
+  }
+}
--- src/Framework/Core/Form/Control/index.js
+++ src/Framework/Core/Form/Control/index.js
@@ -1,9 +1,9 @@
 import { route as buttonRoute } from './Button';
-// import { route as linkRoute } from './Link';
+import { route as linkRoute } from './Link';
 
 export const routes = [
   buttonRoute,
-  // linkRoute,
+  linkRoute,
 ];
 
 export default from './Control.component';
