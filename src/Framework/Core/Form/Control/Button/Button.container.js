4f91691cbeedabe7b0d33b500ce4d188c0f8a935
psychobolt <psychobolt@gmail.com>
2017-05-29 13:54:35 -0700

    fix merge conflicts issue

--- src/Framework/Core/Form/Control/Button/Button.container.js
+++ src/Framework/Core/Form/Control/Button/Button.container.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
-import FormComponent from '../../Form.component';
-import ButtonSchema from './Button.schema';
-import i18n from './Button.i18n';
-
-=======
 // framework
 import { Form } from '../../../Form';
 import schema from './Button.schema';
 import i18n from './Button.i18n';
->>>>>>> master
 import { getLocale } from '../../../../../Shared/utils';
 
 const messages = i18n[getLocale()];
@@ -19,18 +12,8 @@ export default class ButtonContainer extends Component {
   static navigationOptions = {
     drawerLabel: messages.title,
     title: messages.title,
-<<<<<<< HEAD
-  };
-
-  render() {
-    return (
-      <FormComponent messages={messages} schema={ButtonSchema} />
-    );
-=======
-  }
 
   render() {
     return <Form schema={schema} messages={messages} />;
->>>>>>> master
   }
 }
--- src/Framework/Core/Form/Control/Link/Link.container.js
+++ src/Framework/Core/Form/Control/Link/Link.container.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
-
-<<<<<<< HEAD
-import FormComponent from '../../Form.component';
-import LinkSchema from './Link.schema';
-import i18n from './Link.i18n';
-
-=======
 // framework
 import { Form } from '../../../Form';
 import schema from './Link.schema';
 import i18n from './Link.i18n';
->>>>>>> master
 import { getLocale } from '../../../../../Shared/utils';
 
 const messages = i18n[getLocale()];
 
-<<<<<<< HEAD
-export default class ButtonContainer extends Component {
-  static navigationOptions = {
-    drawerLabel: messages.title,
-    title: messages.title,
-  };
-
-  render() {
-    return (
-      <FormComponent messages={messages} schema={LinkSchema} />
-    );
-=======
 export default class LinkContainer extends Component {
   static navigationOptions = {
     drawerLabel: messages.title,
@@ -35,6 +15,5 @@ export default class LinkContainer extends Component {
 
   render() {
     return <Form schema={schema} messages={messages} />;
->>>>>>> master
   }
 }
