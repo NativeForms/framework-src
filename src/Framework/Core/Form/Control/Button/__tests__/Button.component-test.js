07aaaf0bf1a073c22b1b04052ef49620f1c311cb
nan2iz <nan2iz.ch@gmail.com>
2017-07-24 20:49:00 -0700

    update test with renderer

--- src/Framework/Core/Form/Control/Button/__tests__/Button.component-test.js
+++ src/Framework/Core/Form/Control/Button/__tests__/Button.component-test.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Button } from 'native-base';
+import { Button, Text } from 'native-base';
 import renderer from 'react-test-renderer';
 
 
 //button-framework
 import ButtonComponent from '../Button.component';
-//import i18n from '../Button.i18n';
+import i18n from '../Button.i18n';
+
+const messages = i18n.en;
+
 
 describe('components <ButtonComponent />', () => {
 
-  it('renders lebel button', () => {
+  it('renders lebel button correctly', () => {
     const tree = renderer.create(
        <Button>
-           Test
+           <Text>{messages.myLabel}</Text>
        </Button>
    ).toJSON();
     expect(tree).toMatchSnapshot();
