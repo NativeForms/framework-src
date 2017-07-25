825026b7a0807180b143e8831bec1fc671a9ce88
nan2iz <nan2iz.ch@gmail.com>
2017-07-25 09:33:18 -0700

    add snapshot test for container

---
+++ src/Framework/Core/Form/Control/Button/__tests__/Button.container-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ButtonContainer from '../Button.container';
+
+describe('container <ButtonContainer />', () => {
+  it('container rander correctly', () => {
+    const wrapper = shallow(
+      <ButtonContainer />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
