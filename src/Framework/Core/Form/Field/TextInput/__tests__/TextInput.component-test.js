0b10abd021f9a8d047476cad3038c6b8db9fb6c9
nan2iz <nan2iz.ch@gmail.com>
2017-08-01 14:26:55 -0700

    Add full coverage for textInput component

--- src/Framework/Core/Form/Field/TextInput/__tests__/TextInput.component-test.js
+++ src/Framework/Core/Form/Field/TextInput/__tests__/TextInput.component-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import injectIntl from 'react-intl';
+import { Input, Icon } from 'native-base';
 
 // framework
 import TextInputComponent from '../TextInput.component';
@@ -12,17 +12,36 @@ import { mountWithIntl } from '../../../../../../Shared/intl-enzyme-test-helper'
 const message = i18n.en;
 const propsArray = schema.attributes.formComponents;
 
-
 describe('components <TextInputComponent />', () => {
   let wrapper;
   propsArray.forEach((props) => {
     it('renders text input component correctly', () => {
       wrapper = mountWithIntl(<TextInputComponent {...props.attributes} />, {}, message);
       expect(wrapper).toMatchSnapshot();
-      if (props.attributes.clearButton) {
+    });
+    if (props.attributes.clearButton) {
+      it('clearButton works correctly', () => {
         wrapper = mountWithIntl(<TextInputComponent {...props.attributes} />, {}, message);
-        expect(wrapper.find('value')).toHaveLength(0);
-      }
+        wrapper.find(Icon).props().onPress();
+        expect(wrapper.find(Input).props().value).toHaveLength(0);
+      });
+    }
+    it('set value works correctly', () => {
+      wrapper = mountWithIntl(<TextInputComponent {...props.attributes} />, {}, message);
+      const mockValue = 'test input text';
+      wrapper.find(Input).props().onChangeText(mockValue);
+      expect(wrapper.find(Input).props().value).toMatch(mockValue);
+    });
+    it('measure works correctly', () => {
+      wrapper = mountWithIntl(<TextInputComponent {...props.attributes} />, {}, message);
+      const mockHight = {
+        nativeEvent: {
+          contentSize: { height: 10 }
+        } };
+      wrapper.find(Input).props().onContentSizeChange(mockHight);
+      expect(
+        wrapper.find(Input).props().style.height
+      ).toEqual(mockHight.nativeEvent.contentSize.height);
     });
   });
 });
