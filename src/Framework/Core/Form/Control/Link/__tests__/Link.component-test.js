bafeebc3f47ff1962da559f81c392cfd6beb921e
nan2iz <nan2iz.ch@gmail.com>
2017-08-08 23:18:13 -0700

    Update link component test

--- src/Framework/Core/Form/Control/Link/__tests__/Link.component-test.js
+++ src/Framework/Core/Form/Control/Link/__tests__/Link.component-test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { TouchableHighlight, Linking } from 'react-native';
 import LinkComponent from '../Link.component';
 import i18n from '../Link.i18n';
 
@@ -28,5 +30,17 @@ describe('components <LinkComponent />', () => {
       <LinkComponent {...props} />
     );
     expect(wrapper).toMatchSnapshot();
+  });
+  it('linkTo function works correctly', () => {
+    //let stubLink = sinon.stub(Linking, 'canOpenURL');
+    //stubLink.withArgs(mockRoute).returns(true);
+    const props = {
+      label: messages.title,
+      route: mockRoute,
+    };
+    const wrapper = shallow(
+      <LinkComponent {...props} />
+    );
+    wrapper.find(TouchableHighlight).props().onPress();
   });
 });
