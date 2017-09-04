0f058eb854bcaf33775c913926ffad2d681c5bb1
nan2iz <nan2iz.ch@gmail.com>
2017-09-03 23:22:59 -0700

    Update Rendering

--- src/Framework/Core/Form/Field/Selection/Selection.component.js
+++ src/Framework/Core/Form/Field/Selection/Selection.component.js
@@ -1,18 +1,65 @@
-import React, { Component } from 'react';
-//import { Text } from 'react-native';
-import { CheckBox, ListItem } from 'native-base';
+import React, { Component, PropTypes } from 'react';
+import { List, ListItem, CheckBox, Body, Text, Radio } from 'native-base';
+/*import { CheckBox } from 'react-native-elements';*/
 import { injectIntl } from 'react-intl';
 
 class SelectionComponent extends Component {
-  /*static propTypes = {
+  static propType = {
     label: PropTypes.string,
-    selectionList: PropTypes.object,
-    radioBoxType: PropTypes.bool
-  }*/
+    radionBoxType: PropTypes.bool,
+    selectionList: PropTypes.object
+  }
+
+  static defaultProps = {
+    label: null,
+    radionBoxType: false,
+    selectionList: null
+  }
+
   render() {
+    const { label, radionBoxType, selectionList, color, uid} = this.props;
+    const childItems = selectionList.map((item) =>
+      <SelectionChild key={uid} props={item} />
+    );
+    /*const props = {
+
+    };*/
     return (
-      <ListItem>
-        <CheckBox checked={false} />
+      <List>
+        {label &&
+          <ListItem itemDivider>
+            <Body>
+              <Text>{ label }</Text>
+            </Body>
+          </ListItem>
+        }
+        { childItems }
+      </List>
+    );
+  }
+}
+
+/* REMOVE to ANOTHER FILE */
+class SelectionChild extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: this.props.checked,
+    };
+    this.clickSelection = this.clickSelection.bind(this);
+  }
+  clickSelection() {
+    this.setState({ checked: !this.state.checked });
+  }
+
+  render(){
+    return (
+      <ListItem key={this.props.uid}>
+        <CheckBox checked={this.state.checked} onPress={this.clickSelection}>
+          <Body>
+            <Text>{ this.props.itemLabel }</Text>
+          </Body>
+        </CheckBox>
       </ListItem>
     );
   }
--- src/Framework/Core/Form/Field/Selection/Selection.schema.js
+++ src/Framework/Core/Form/Field/Selection/Selection.schema.js
@@ -6,13 +6,73 @@ export default {
     label: 'Selection Example',
     formComponents: [
       {
-        uid: 'Selection1',
+        uid: 'noHeaderSelection',
         type: 'selection',
         category: 'field',
         attributes: {
-          label: 'myLabel',
+          radionBoxType: false,
+          selectionList: [
+            {
+              uid: 'child1',
+              itemLabel: 'No-Header Selection 1',
+              checked: false,
+            },
+            {
+              uid: 'child2',
+              itemLabel: 'No-Header Selection 2]',
+              checked: false,
+            }
+          ]
         }
-      }
+      },
+      {
+        uid: 'selectionExample',
+        type: 'selection',
+        category: 'field',
+        attributes: {
+          label: 'Selection Main Label',
+          radionBoxType: false,
+          selectionList: [
+            {
+              uid: 'child1',
+              itemLabel: 'item1',
+              checked: false,
+            },
+            {
+              uid: 'child2',
+              itemLabel: 'item2',
+              checked: false,
+            },
+            {
+              uid: 'child3',
+              itemLabel: 'item2',
+              checked: false,
+            }
+          ]
+        }
+      },
+      {
+        uid: 'radioSelectionExample',
+        type: 'selection',
+        category: 'field',
+        attributes: {
+          label: 'Radio Box Main Label',
+          radionBoxType: false,
+          color: 'red',
+          selectionList: [
+            {
+              uid: 'child1',
+              itemLabel: 'item1',
+              checked: false,
+            },
+            {
+              uid: 'child2',
+              itemLabel: 'item2',
+              checked: false,
+            }
+          ]
+        }
+      },
     ]
   }
 };
